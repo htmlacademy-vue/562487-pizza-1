8b85778717ab6b13747222791b08cb59d02a2f69
alenakazarina1 <alenakazarina1@gmail.com>
2021-12-09 14:24:27 +0300

    added layouts

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-layout">
+    <AppLayoutHeader />
+    <main class="content">
+      <IndexHome />
+    </main>
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+import IndexHome from "@/views/Index";
+
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+    IndexHome,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
