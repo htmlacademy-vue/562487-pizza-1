b37ae913d40f83682a3313036cdf9cdff742b950
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-01-18 17:10:58 +0300

    updated app

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/plugins/ui";
+import "@/plugins/vuePlugins";
 import router from "@/router";
+import store from "@/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
