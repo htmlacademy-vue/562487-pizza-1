ae644e3bc6dda61b4dd9db72f2c348ad18382f68
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-02-09 13:42:07 +0300

    removed deliveries and ingredients

--- src/frontend/src/common/enums/index.js
+++ src/frontend/src/common/enums/index.js
@@ -1,9 +1,7 @@
 import PizzaDoughs from "./doughs";
 import PizzaSizes from "./sizes";
 import PizzaSauces from "./sauces";
-import PizzaIngredients from "./ingredients";
 import PizzaFoundations from "./foundations";
-import Deliveries from "./deliveries";
 import NotificationTypes from "./notificationTypes";
 import Resources from "./resources";
 
@@ -11,9 +9,7 @@ export {
   PizzaDoughs,
   PizzaSizes,
   PizzaSauces,
-  PizzaIngredients,
   PizzaFoundations,
-  Deliveries,
   NotificationTypes,
   Resources,
 };
