c2f587a024aceb0e53a5b19695b71c7389f30336
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-09 12:40:20 +0300

    added cart popups transitions

--- src/frontend/src/modules/cart/components/CartPopup.vue
+++ src/frontend/src/modules/cart/components/CartPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <PopupLayout close="closePopup">
+  <PopupLayout @close="$emit('close')">
     <div class="popup__title">
       <h2 class="title">Спасибо за заказ</h2>
     </div>
@@ -11,15 +11,8 @@
 </template>
 
 <script>
-import PopupLayout from "@/modules/popup/components/PopupLayout";
-import PopupButton from "@/modules/popup/components/PopupButton";
-
 export default {
   name: "CartPopup",
-  components: {
-    PopupLayout,
-    PopupButton,
-  },
   mounted() {
     this.$refs.ok.focus();
   },
--- src/frontend/src/views/Cart.vue
+++ src/frontend/src/views/Cart.vue
@@ -6,19 +6,37 @@
           <h1 class="title title--big">Корзина</h1>
         </div>
 
-        <div v-if="!order.pizzas.length" class="sheet cart__empty">
+        <div v-if="isEmpty" class="sheet cart__empty">
           <p>В корзине нет ни одного товара</p>
         </div>
 
-        <template v-else>
-          <CartPizzas :pizzas="order.pizzas" />
-          <CartMisc :orderMisc="order.misc" />
+        <div v-else>
+          <CartPizzas :pizzas="orderPizzas" @deletePizza="startDeletePizza" />
+          <CartMisc :orderMisc="orderMisc" />
           <CartDelivery />
-        </template>
+        </div>
       </div>
     </main>
     <CartFooter :isSubmitDisabled="isInvalid || isSubmitting" />
-    <CartPopup v-if="isPopupShowed" @close="closePopup" />
+
+    <PopupTransition hasCallback @leave="leavePage">
+      <CartPopup
+        v-if="isSuccessPopupShowed"
+        @close="isSuccessPopupShowed = false"
+      />
+    </PopupTransition>
+    <PopupTransition>
+      <ConfirmPopup
+        v-if="isConfirmPopupShowed"
+        @confirm="confirmDeletePizza"
+        @cancel="isConfirmPopupShowed = false"
+      >
+        <h2 class="title">
+          Удалить пиццу {{ orderPizzaNameById(pizzaIdToDelete) }}?
+        </h2>
+        <p>После удаления пицца не сохранится.</p>
+      </ConfirmPopup>
+    </PopupTransition>
   </form>
 </template>
 
@@ -31,17 +49,11 @@ import CartFooter from "@/modules/cart/components/CartFooter";
 import CartPopup from "@/modules/cart/components/CartPopup";
 import {
   SET_CART_ENTITY,
-  SET_CART_ORDER_ENTITY,
   RESET_CART,
+  DELETE_PIZZA,
 } from "@/store/mutations-types";
 import { BASE_DELIVERIES } from "@/common/constants";
-
-const createDeliveries = (items) => {
-  return items.map((it) => ({
-    id: it.id,
-    name: it.name,
-  }));
-};
+import { Pizza } from "@/common/models";
 
 export default {
   name: "Cart",
@@ -54,17 +66,29 @@ export default {
   },
   data() {
     return {
-      isPopupShowed: false,
+      isSuccessPopupShowed: false,
+      isConfirmPopupShowed: false,
       isSubmitting: false,
+      pizzaIdToDelete: null,
     };
   },
   computed: {
-    ...mapState("Auth", ["user", "addresses"]),
+    ...mapState("Auth", ["user"]),
     ...mapState("Orders", ["orders"]),
-    ...mapState("Cart", ["delivery", "phone", "order", "deliveries"]),
+    ...mapState("Cart", [
+      "delivery",
+      "phone",
+      "orderPizzas",
+      "orderMisc",
+      "orderAddress",
+    ]),
     ...mapGetters("Auth", ["isUserAddress"]),
-    ...mapGetters("Cart", ["totalSum"]),
+    ...mapGetters("Cart", ["totalSum", "miscById", "orderPizzaNameById"]),
     ...mapGetters("Orders", ["getOrderById"]),
+
+    isEmpty() {
+      return this.orderPizzas.length === 0;
+    },
 
     isInvalid() {
       if (this.delivery === BASE_DELIVERIES[0].id) {
@@ -72,38 +96,59 @@ export default {
       }
       return (
         !this.totalSum ||
-        !this.order.address.street ||
-        !this.order.address.building
+        !this.orderAddress?.street ||
+        !this.orderAddress?.building
       );
     },
   },
 
   async created() {
-    if (this.user && this.order.pizzas.length) {
-      await this.queryAddresses();
-    }
     const orderId = this.$route.params.id;
-    if (orderId) {
+    if (this.user && orderId) {
+      await this.queryAddresses();
       this.setupCartWithOrder(orderId);
-    } else {
-      this.setupDeliveries();
+      return;
+    }
+    if (this.user && !this.isEmpty) {
+      await this.queryAddresses();
     }
   },
   methods: {
     ...mapMutations("Cart", {
       setCartEntity: SET_CART_ENTITY,
-      setCartOrderEntity: SET_CART_ORDER_ENTITY,
       resetCart: RESET_CART,
+      deletePizza: DELETE_PIZZA,
     }),
     ...mapActions("Auth", ["queryAddresses"]),
     ...mapActions("Orders", ["createOrder"]),
 
-    setupDeliveries() {
-      const userDeliveries = createDeliveries(this.addresses);
-      this.setCartEntity({
-        entity: "deliveries",
-        value: [...BASE_DELIVERIES, ...userDeliveries],
-      });
+    setCartOrderPizzas(value) {
+      this.setCartEntity({ entity: "orderPizzas", value });
+    },
+    setCartOrderMisc(value) {
+      this.setCartEntity({ entity: "orderMisc", value });
+    },
+    setAddress(value) {
+      this.setCartEntity({ entity: "orderAddress", value });
+    },
+    setDelivery(value) {
+      this.setCartEntity({ entity: "delivery", value });
+    },
+    setPhone(value) {
+      this.setCartEntity({ entity: "phone", value });
+    },
+
+    startDeletePizza(pizzaId) {
+      this.pizzaIdToDelete = pizzaId;
+      this.isConfirmPopupShowed = true;
+    },
+
+    confirmDeletePizza() {
+      this.isConfirmPopupShowed = false;
+      this.deletePizza(this.pizzaIdToDelete);
+      if (!this.orderPizzas.length) {
+        this.resetCart();
+      }
     },
 
     setupCartWithOrder(orderId) {
@@ -112,47 +157,37 @@ export default {
         this.$route.push("/cart");
         return;
       }
-      this.setupDeliveries();
-      this.setCartEntity({ entity: "order", value: orderToEdit });
-      this.setCartEntity({ entity: "phone", value: orderToEdit.phone });
-      if (orderToEdit.address) {
-        this.setCartEntity({
-          entity: "delivery",
-          value: orderToEdit.address.id,
-        });
-      }
-    },
-
-    closePopup() {
-      if (this.user) {
-        this.$router.push("/orders");
-      } else {
-        this.$router.push("/");
-      }
+      this.setDelivery(orderToEdit.orderAddress?.id || BASE_DELIVERIES[0].id);
+      this.setPhone(orderToEdit.phone);
+      this.setAddress(orderToEdit.orderAddress);
+      const cartOrderPizzas = Pizza.parseItems(orderToEdit.orderPizzas);
+      this.setCartOrderPizzas(cartOrderPizzas);
+      this.setCartOrderMisc(orderToEdit.orderMisc);
     },
 
     async submit() {
       this.isSubmitting = true;
-      const addressId = this.order.address?.id;
-      const isUserAddress = this.isUserAddress(addressId);
-      const address = addressId
-        ? isUserAddress
-          ? this.order.address
-          : this.order.address.toOrderRaw()
-        : null;
+      const newOrder = {
+        userId: this.user ? this.user.id : null,
+        phone: this.phone,
+        pizzas: this.orderPizzas.map((pizza) => pizza.toRaw()),
+        misc: this.orderMisc,
+        address: this.orderAddress,
+      };
+
       try {
-        await this.createOrder({
-          userId: this.user ? this.user.id : null,
-          phone: this.phone,
-          pizzas: this.order.pizzas.map((pizza) => pizza.toRaw()),
-          misc: this.order.misc,
-          address,
-        });
+        await this.createOrder(newOrder);
         this.resetCart();
-        this.isPopupShowed = true;
-      } catch {
+        this.isSuccessPopupShowed = true;
+      } catch (err) {
+        console.log(err);
         this.isSubmitting = false;
       }
+    },
+
+    leavePage() {
+      const nextRoute = this.user ? "/orders" : "/";
+      this.$router.push(nextRoute);
     },
   },
 };
