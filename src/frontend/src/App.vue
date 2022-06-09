f3a14e1bc2fdeac810e7aa886ac7ecd0a1bc5578
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-09 12:50:30 +0300

    updated

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -11,18 +11,84 @@ import { setAuth } from "@/common/helpers";
 
 export default {
   name: "App",
-  created() {
+  async created() {
     window.onerror = function (msg, url, line, col, error) {
       console.error(error);
     };
     if (this.$jwt.getToken()) {
       setAuth(this.$store);
     }
-    this.$store.dispatch("init");
+    await this.$store.dispatch("init");
   },
 };
 </script>
 
 <style lang="scss">
 @import "~@/assets/scss/app";
+
+@keyframes slide {
+  0% {
+    transform: translateX(10%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes sidebar-slide {
+  0% {
+    opacity: 0;
+    transform: translateX(-25px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes zoom-in {
+  0% {
+    transform: translate(-50%, -50%) scale(0.3);
+    opacity: 0;
+  }
+  50% {
+    transform: translate(-50%, -50%);
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 100%;
+  }
+}
+
+@keyframes slide-up {
+  0% {
+    transform: translate(-50%, -50%) translateY(-10%);
+    opacity: 0;
+  }
+  100% {
+    transform: translate(-50%, -50%) translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes roll-in {
+  0% {
+    transform: scale(0) rotateZ(0deg) translateX(-250px);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1) rotateZ(360deg) translateX(0px);
+    opacity: 1;
+  }
+}
 </style>
