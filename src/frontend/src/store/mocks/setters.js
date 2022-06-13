45018082e4a026cafb22d87bc93b67586fa6741c
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-13 15:22:50 +0300

    added store mocks functions

---
+++ src/frontend/src/store/mocks/setters.js
@@ -0,0 +1,35 @@
+import { SET_ENTITY } from "@/store/mutations-types";
+import { Dough, Sauce, Size, Ingredient } from "@/common/models";
+import pizzaData from "@/static/pizza.json";
+
+export const setDoughs = (store) => {
+  store.commit(SET_ENTITY, {
+    module: "Builder",
+    entity: "doughs",
+    value: Dough.parseItems(pizzaData.dough),
+  });
+};
+
+export const setSauces = (store) => {
+  store.commit(SET_ENTITY, {
+    module: "Builder",
+    entity: "sauces",
+    value: Sauce.parseItems(pizzaData.sauces),
+  });
+};
+
+export const setSizes = (store) => {
+  store.commit(SET_ENTITY, {
+    module: "Builder",
+    entity: "sizes",
+    value: Size.parseItems(pizzaData.sizes),
+  });
+};
+
+export const setIngredients = (store) => {
+  store.commit(SET_ENTITY, {
+    module: "Builder",
+    entity: "ingredients",
+    value: Ingredient.parseItems(pizzaData.ingredients),
+  });
+};
