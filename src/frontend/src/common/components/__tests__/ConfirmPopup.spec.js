243394fac1aa3fa706b51d8d4ade70069fced33d
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-14 18:41:43 +0300

    added ConfirmPopup test

---
+++ src/frontend/src/common/components/__tests__/ConfirmPopup.spec.js
@@ -0,0 +1,71 @@
+import { shallowMount } from "@vue/test-utils";
+import ConfirmPopup from "@/common/components/ConfirmPopup";
+import PopupLayout from "@/common/components/PopupLayout";
+import PopupOverlay from "@/common/components/PopupOverlay";
+import PopupButton from "@/common/components/PopupButton";
+
+describe("ConfirmPopup", () => {
+  const confirmBtnSelector = "[data-test='confirm-btn'] a";
+  const cancelBtnSelector = "[data-test='cancel-btn'] a";
+  const propsData = { isSubmitting: true };
+  const stubs = {
+    PopupLayout,
+    PopupOverlay,
+    PopupButton,
+  };
+  const slots = { default: "content" };
+
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = shallowMount(ConfirmPopup, options);
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders out the slot content", () => {
+    createComponent({ stubs, slots });
+    expect(wrapper.html()).toContain(slots.default);
+  });
+
+  it("renders out 2 buttons", () => {
+    createComponent({ stubs, slots });
+    expect(wrapper.find(confirmBtnSelector).exists()).toBe(true);
+    expect(wrapper.find(cancelBtnSelector).exists()).toBe(true);
+  });
+
+  it("emits confirm on confirm button click", async () => {
+    createComponent({ stubs });
+    const confirmBtn = wrapper.find(confirmBtnSelector);
+    await confirmBtn.trigger("click");
+    expect(wrapper.emitted().confirm).toBeTruthy();
+  });
+
+  it("does not emit confirm on confirm button click when prop isSubmitting", async () => {
+    createComponent({ stubs, propsData });
+    const confirmBtn = wrapper.find(confirmBtnSelector);
+    await confirmBtn.trigger("click");
+    expect(wrapper.emitted().confirm).toBeFalsy();
+  });
+
+  it("emits cancel on cancel button click", async () => {
+    createComponent({ stubs });
+    const cancelBtn = wrapper.find(cancelBtnSelector);
+    await cancelBtn.trigger("click");
+    expect(wrapper.emitted().cancel).toBeTruthy();
+  });
+
+  it("emits cancel when popup layout emits close", async () => {
+    createComponent({ stubs });
+    const overlay = wrapper.find(".overlay");
+    await overlay.trigger("click");
+    expect(wrapper.emitted().cancel).toBeTruthy();
+  });
+
+  it("renders button with cancel ref", () => {
+    createComponent({ stubs });
+    const btn = wrapper.findComponent({ ref: "cancel" });
+    expect(btn.exists()).toBe(true);
+  });
+});
