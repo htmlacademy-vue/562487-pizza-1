b2836b4f0aa8fe65228e6c23ae7b5d1ade19ee9d
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-14 12:47:17 +0300

    added PopupLayout test

---
+++ src/frontend/src/common/components/__tests__/PopupLayout.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount } from "@vue/test-utils";
+import PopupLayout from "@/common/components/PopupLayout";
+import PopupOverlay from "@/common/components/PopupOverlay";
+
+describe("PopupLayout", () => {
+  const stubs = {
+    PopupOverlay,
+  };
+  const slots = { default: "content" };
+
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = shallowMount(PopupLayout, options);
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders out the slot content", () => {
+    createComponent({ stubs, slots });
+    expect(wrapper.html()).toContain(slots.default);
+  });
+
+  it("renders close button with close ref", () => {
+    createComponent({ stubs });
+    const closeBtn = wrapper.findComponent({ ref: "close" });
+    expect(closeBtn.exists()).toBe(true);
+  });
+
+  it("emits close on close button click", async () => {
+    createComponent({ stubs });
+    const closeBtn = wrapper.find(".close");
+    await closeBtn.trigger("click");
+    expect(wrapper.emitted().close.length).toBe(1);
+  });
+
+  it("emits close on overlay click", async () => {
+    createComponent({ stubs });
+    const overlay = wrapper.find(".overlay");
+    await overlay.trigger("click");
+    expect(wrapper.emitted().close.length).toBe(1);
+  });
+
+  it("emits close on escape keydown", async () => {
+    createComponent({ stubs });
+    const overlay = wrapper.find(".overlay");
+    await overlay.trigger("keydown", { key: "Escape" });
+    expect(wrapper.emitted().close.length).toBe(1);
+  });
+});
