e2e30d416db502b4a4ecbe346329a40f231b4aba
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-14 12:10:46 +0300

    added PopupOverlay test

---
+++ src/frontend/src/common/components/__tests__/PopupOverlay.spec.js
@@ -0,0 +1,38 @@
+import { shallowMount } from "@vue/test-utils";
+import PopupOverlay from "@/common/components/PopupOverlay";
+
+describe("PopupOverlay", () => {
+  const slots = { default: "content" };
+  const listeners = { click: null, keydown: null };
+
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = shallowMount(PopupOverlay, options);
+  };
+
+  beforeEach(() => {
+    listeners.click = jest.fn();
+    listeners.keydown = jest.fn();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders out the slot content", () => {
+    createComponent({ slots });
+    expect(wrapper.html()).toContain(slots.default);
+  });
+
+  it("raises the click event on click", async () => {
+    createComponent({ listeners });
+    await wrapper.find(".overlay").trigger("click");
+    expect(listeners.click).toHaveBeenCalled();
+  });
+
+  it("raises the keydown event on keydown", async () => {
+    createComponent({ listeners });
+    await wrapper.find(".overlay").trigger("keydown");
+    expect(listeners.keydown).toHaveBeenCalled();
+  });
+});
