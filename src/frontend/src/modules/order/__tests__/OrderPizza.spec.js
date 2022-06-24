c04da610f1281defaa220aa2088ecaf60f36f898
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-06-24 16:23:15 +0300

    added OrderPizza with test

---
+++ src/frontend/src/modules/order/__tests__/OrderPizza.spec.js
@@ -0,0 +1,52 @@
+import { createLocalVue, mount } from "@vue/test-utils";
+import Vuex from "vuex";
+import { generateMockStore } from "@/store/mocks";
+import OrderPizza from "../components/OrderPizza";
+import { setUIComponents } from "@/plugins/ui";
+import {
+  setDoughs,
+  setSauces,
+  setSizes,
+  setIngredients,
+  testOrder,
+} from "@/store/mocks/setters";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+setUIComponents(localVue);
+
+describe("OrderPizza", () => {
+  const propsData = {
+    pizza: testOrder.orderPizzas[0],
+  };
+  let store;
+  let wrapper;
+  const createComponent = (options) => {
+    wrapper = mount(OrderPizza, options);
+  };
+
+  beforeEach(() => {
+    store = generateMockStore();
+    setDoughs(store);
+    setSauces(store);
+    setSizes(store);
+    setIngredients(store);
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("renders out order pizza", () => {
+    createComponent({ localVue, store, propsData });
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("renders out pizza quantity and price", () => {
+    createComponent({ localVue, store, propsData });
+    const price = wrapper.find("[data-test='pizza-price']");
+    const { quantity } = propsData.pizza;
+    const pizzaPrice = store.getters["Builder/pizzaPrice"](propsData.pizza);
+    expect(price.text()).toContain(`${quantity} х ${pizzaPrice}`);
+  });
+});
