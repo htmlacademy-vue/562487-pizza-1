604bf871bcec1552cc026dd77ffd53965c3c922a
Alena Kazarina <31925531+alenakazarina@users.noreply.github.com>
2022-07-13 11:08:53 +0300

    fixed

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -55,13 +55,6 @@ export default {
   }
 }
 
-@keyframes sidebar-leave {
-  100% {
-    opacity: 0;
-    transform: translateX(-25px);
-  }
-}
-
 @keyframes zoom-in {
   0% {
     transform: translate(-50%, -50%) scale(0.3);
@@ -81,28 +74,6 @@ export default {
   }
   100% {
     opacity: 100%;
-  }
-}
-
-@keyframes slide-up {
-  0% {
-    transform: translate(-50%, -50%) translateY(-10%);
-    opacity: 0;
-  }
-  100% {
-    transform: translate(-50%, -50%) translateY(0);
-    opacity: 1;
-  }
-}
-
-@keyframes roll-in {
-  0% {
-    transform: scale(0) rotateZ(0deg) translateX(-250px);
-    opacity: 0;
-  }
-  100% {
-    transform: scale(1) rotateZ(360deg) translateX(0px);
-    opacity: 1;
   }
 }
 </style>
